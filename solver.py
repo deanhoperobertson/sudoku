6668e11875f7c18802fb896489e8592491b89a0a
Dean Hope Robertson <deanhoperobertson@gmail.com>
2020-03-31 18:13:57 +0100

    Consolidated Code
    removed roundup function
    removed rounddown function
    edited dataframe index to make searching easier

--- solver.py
+++ solver.py
@@ -11,37 +11,8 @@ df2 = pd.DataFrame(np.array([["", "", 7,5,"",9,"",3,""],
                             ["",7,"","","","","",1,""],
                             [8,"","","",5,"",2,9,""],
                             ["",5,"",1,"",2,4,"",7]]),
-                   columns=[i for i in range(1,10)],
-                   index=[i for i in range(1,10)])
-
-def roundup(x):
-    '''
-    Rounds down coordonate the closest block limit
-
-    Args:
-        x: (int) input coordinate
-
-    Returns:
-        int: rouded down value
-    '''
-    if x ==1 : return 3
-    else:  return int(math.ceil(x / 3.0)) * 3
-
-def rounddown(x):
-    '''
-    Rounds up coordonate the closest block limit
-
-    Args:
-        x: (int) input coordinate
-
-    Returns:
-        int: rouded up value
-    '''
-    if x%3 ==0: return x-2
-    else: 
-        val = round(x - (x%3.5))
-        if val ==0: return 1 
-        else: return val
+                   columns=[i for i in range(0,9)],
+                   index=[i for i in range(0,9)])
 
 def get_block(df,row,col):
     '''
@@ -56,8 +27,12 @@ def get_block(df,row,col):
         list: all the numbers in a block
     '''
     output=[]
-    for c in range(rounddown(col),roundup(col)+1):
-        for r in range(rounddown(row),roundup(row)+1):
+    
+    x_start, y_start = 3*(row//3), 3*(col//3)
+    x_end, y_end = x_start + 3, y_start + 3
+    
+    for c in range(y_start,y_end):
+        for r in range(x_start,x_end):
             output.append(df2[c][r])
     return [int(i) for i in output if i !=""]
 
@@ -122,13 +97,13 @@ def create_options_df(df):
         dataframe: matrix with options in each cell
     '''
 
-    df_output = pd.DataFrame(index=[i for i in range(1,10)], columns=[i for i in range(1,10)])
-    for row in range(1,10):
-        for col in range(1,10):
+    df_output = pd.DataFrame(index=[i for i in range(0,9)], columns=[i for i in range(0,9)])
+    for row in range(0,9):
+        for col in range(0,9):
             df_output[col][row]=options_finder(df,row,col)
     return df_output
 
-def location_finder(df):
+def location_finder(df,value):
     '''
     Find the location of cells whihc has only 1 possible option
 
@@ -139,9 +114,9 @@ def location_finder(df):
         list: coordinates of cells
     '''
     output = []
-    for row in range(1,10):
-        for col in range(1,10):
-            if df[col][row] ==1:
+    for row in range(0,9):
+        for col in range(0,9):
+            if df[col][row] ==value:
                 output.append([row,col])
     return output
 
@@ -157,7 +132,7 @@ def solve(df):
     '''
     action = True
     while action == True:
-        coords=location_finder(create_options_df(df))
+        coords=location_finder(create_options_df(df),1)
         if coords:
             for i in coords:
                 row = i[0]
